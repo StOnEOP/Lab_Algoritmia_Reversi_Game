b619f39b85c747292542764cb1a36bc6931c9086
Drex1904 <a87999@alunos.uminho.pt>
2019-05-20 14:48:44 +0100

    comentei a isover e suas amigas e a vencedor

--- jogar.c
+++ jogar.c
@@ -38,14 +38,14 @@ int jogadaValida(ESTADO e, int linha,int coluna){ //
         return 1;
     }else return 0;
 }
-int isover(ESTADO e){
+int isover(ESTADO e){//retorna um inteiro >0 se estiver acabdo e 0 se nao estiver no fim
     e=retiraValida(e);
     e=validasJogada(e);
-   int r=isovervazias(e)+isoversemjogadas(e);
+   int r=isovervazias(e)+isoversemjogadas(e);//verifica qual dos tipos de over tem somando-os
    e=retiraValida(e);
     return r;
 }
-int isovervazias(ESTADO e) {
+int isovervazias(ESTADO e) {//verifica se o tabuleiro esta completamente preenchido
     int cont=0;int r=0;
   for(int i=0;i<8;i++) {
       for (int j = 0; j < 8 && e.grelha[i][j] !=VAZIA && e.grelha[i][j] != VALIDA; j++){
@@ -55,7 +55,7 @@ int isovervazias(ESTADO e) {
   }
 
 
-  if(cont==64){
+  if(cont==64){//esta preenchido se estiver 64 peças no tabuleiro
       r=1;
   }
 
@@ -63,17 +63,17 @@ int isovervazias(ESTADO e) {
 
 }
 
-int isoversemjogadas(ESTADO e){
+int isoversemjogadas(ESTADO e){//vai a funçao temvalidas vereficar se ambos os jogadores tem ou nao jogadas validas
 
-    if(temvalidas(e,e.peca)){
+    if(temvalidas(e,e.peca)){//se tiver validas recebe um valor >0 da funçao temvalidas
         return 0;
 
 
-            }else return 1;
+            }else return 1;//se nao tiver jogadas validas retorna 1
 
 }
 
-int temvalidas(ESTADO e,VALOR p) {
+int temvalidas(ESTADO e,VALOR p) {//corre o tabuleiro a procura de jogadas validas
     int r = 0;int t=0;int tot=0;
     e=retiraValida(e);
     e=validasJogada(e);
@@ -87,7 +87,7 @@ int temvalidas(ESTADO e,VALOR p) {
     }
 
     p=inverte(p);
-    e=retiraValida(e);
+    e=retiraValida(e);//troca a peça paera ver se a peça inverça tem ou nao jogadas validas tambem
 
     e=validasJogada(e);
     for(int l=0;l<8;l++){
@@ -98,14 +98,14 @@ int temvalidas(ESTADO e,VALOR p) {
         }
     }
 
-    tot=r + t;
+    tot=r + t;//se tiver jogadas validas tem tot>0 se nao tot=0(que significa nao ha validas)
 
     return tot;
 
 }
 
-void vencedor(ESTADO e){
-    if(isover(e)){
+void vencedor(ESTADO e){//ve quem e o vencedor no jogo
+    if(isover(e)){//se estuver em final de jogo vai ver qual dos jogadores tem mais peças
         int o=0,x=0;
         for(int i=0;i<8;i++) {   // muda linha
             for (int j = 0; j < 8; j++) { // muda coluna
